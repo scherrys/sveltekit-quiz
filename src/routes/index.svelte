306d7ddc5a5465faba33df3e900bd5daee6a19c9
Jenny Scherr <jenny.scherr@svt.se>
2022-05-01 18:13:48 +0200

    add tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold text-center text-gray-800 md:text-3xl">
+    Quiz makers
+</h1>
